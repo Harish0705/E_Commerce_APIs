1ebba79fbce2c913b7796df38949799b219228b1
Harish0705 <harish.balachandar@nzme.co.nz>
2024-09-17 16:01:41 +1200

    create review model

---
+++ models/reviewSchema.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Please provide rating"],
+    },
+    comment: {
+      type: String,
+      required: [true, "Please provide review text"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// User can give only one review for a product.
+reviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+export const Review = mongoose.model("Review", reviewSchema);
