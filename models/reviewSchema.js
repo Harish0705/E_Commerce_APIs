fe55dca58c69b24ae10661979f35c9bf57e385c9
Harish0705 <harish.balachandar@nzme.co.nz>
2024-09-18 12:10:33 +1200

    calculate average rating by using aggregation pipeline

--- models/reviewSchema.js
+++ models/reviewSchema.js
@@ -29,4 +29,37 @@ const reviewSchema = mongoose.Schema(
 // User can give only one review for a product.
 reviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
+//create static
+reviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null, // null as we don't need a id for this group
+        averageRating: { $avg: '$rating' },
+        numerOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+
+  try {
+    await this.model('Product').findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numerOfReviews: result[0]?.numerOfReviews || 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+reviewSchema.post('save', async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
+reviewSchema.post('deleteOne', async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
 export const Review = mongoose.model("Review", reviewSchema);
