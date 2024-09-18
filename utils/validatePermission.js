560f85b96869f3058381b119dd55d85488554145
Harish0705 <harish.balachandar@nzme.co.nz>
2024-09-18 12:10:53 +1200

    add comment

--- utils/validatePermission.js
+++ utils/validatePermission.js
@@ -2,7 +2,7 @@ import { UnauthorizedError } from "../errors/index.js";
 
 export const validatePermission = async (loggedInUser, loggedInuserId) => {
   if (loggedInUser.userRole === "admin") return;
-  console.log(loggedInUser.userId)
+  // compare loggedIn user id and the user id used in the schema for the product, review, order
   if (loggedInUser.userId === loggedInuserId.toString()) return;
   throw new UnauthorizedError("Not autorized to access this url");
 };
