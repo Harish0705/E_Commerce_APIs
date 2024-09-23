04b287d6bf6f20c54712db4d77fa27f1a84c8ca4
Harish0705 <harish.balachandar@nzme.co.nz>
2024-09-24 10:08:31 +1200

    implement email verificationa and refresh token in authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,17 @@
 import { User } from "../models/userSchema.js";
+import { Token } from "../models/userToken.js";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError, UnauthenticatedError } from "../errors/index.js";
-import { createCookie } from "../utils/index.js";
+import {
+  createCookie,
+  sendVerificationEmail,
+  hashToken,
+} from "../utils/index.js";
+import crypto from "crypto";
 
 export const registerUser = async (req, res) => {
   const { name, email, password } = req?.body;
-  // console.log(req.body);
+
   if (!name || !email || !password)
     throw new BadRequestError("Please provide the required fields");
   // check if the email adready exists
@@ -19,7 +25,28 @@ export const registerUser = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = !isFirstAccount ? "user" : "admin";
 
-  await User.create({ name, email, password, role });
+  const emailVerificationToken = crypto.randomBytes(40).toString("hex");
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    emailVerificationToken: hashToken(emailVerificationToken),
+  });
+
+  const localHostOrigin = "http://localhost:3000";
+  // const tempOrigin = req.get('origin');
+  // const protocol = req.protocol;
+  // const host = req.get('host');
+  // const forwardedHost = req.get('x-forwarded-host');
+  // const forwardedProtocol = req.get('x-forwarded-proto');
+  await sendVerificationEmail({
+    name: user.name,
+    email: user.email,
+    emailVerificationToken,
+    origin: localHostOrigin,
+  });
   return res
     .status(StatusCodes.OK)
     .json({ message: "Successfully registered. Please log in to continue" });
@@ -39,15 +66,77 @@ export const loginUser = async (req, res) => {
     userId: user._id,
     role: user.role,
   };
-  await createCookie(res, tokenPayload);
+  // create refresh token
+  let refreshToken = "";
+  // check for existing token
+  const existingToken = await Token.findOne({ user: user._id });
+
+  if (existingToken) {
+    const { isValid } = existingToken;
+    if (!isValid) {
+      throw new UnauthenticatedError("Invalid Credentials");
+    }
+    refreshToken = existingToken.refreshToken;
+    await createCookie(res, tokenPayload, refreshToken);
+    return res.status(StatusCodes.OK).json({ message: "cookie set" });
+  }
+
+  refreshToken = crypto.randomBytes(40).toString("hex");
+  const userAgent = req.headers["user-agent"];
+  const ip = req.ip;
+
+  const userToken = await Token.create({
+    refreshToken,
+    ip,
+    userAgent,
+    user: user._id,
+  });
+  await createCookie(res, tokenPayload, userToken.refreshToken);
   return res.status(StatusCodes.OK).json({ message: "cookie set" });
 };
 
 export const logoutUser = async (req, res) => {
-  //set access token value to logout
+  await Token.findOneAndDelete({ user: req.user.userId });
+
   res.cookie("accessToken", "logout", {
     httpOnly: true,
-    expires: new Date(Date.now() + 1000),
+    expires: new Date(Date.now()),
+  });
+  res.cookie("refreshToken", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
   });
   res.status(StatusCodes.OK).json({ message: "User logged out successfully!" });
+};
+
+export const verifyEmail = async (req, res) => {
+  const { token, email } = req.query;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Verification Failed");
+  }
+
+  if (user.emailVerificationToken !== hashToken(token)) {
+    console.dir();
+    throw new UnauthenticatedError("Verification Failed");
+  }
+
+  user.isVerified = true;
+  user.verifiedDate = Date.now();
+  user.emailVerificationToken = "";
+
+  await user.save();
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "Your Email is Verified. Please log in to continue" });
+};
+export const forgotPassword = async (req, res) => {
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "Please check your email for reset password link" });
+};
+export const resetPassword = async (req, res) => {
+  res.send("reset password");
 };
