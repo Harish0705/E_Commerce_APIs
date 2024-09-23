3ba728326f2f10a0f85aded9d71088a07ada2c0a
Harish0705 <harish.balachandar@nzme.co.nz>
2024-09-24 10:07:38 +1200

    create refresh token and set it as cookie

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,14 @@
 import { creatJwtToken, createCookie, verifyAccessToken } from "./jwt.js";
 import { validatePermission } from "./validatePermission.js";
+import { sendEmail, sendVerificationEmail } from "./email.js";
+import { hashToken } from "./token.js";
 
 export {
     createCookie,
     creatJwtToken,
     verifyAccessToken,
-    validatePermission
+    validatePermission,
+    sendEmail,
+    sendVerificationEmail,
+    hashToken
 }
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,21 +1,30 @@
 import jwt from "jsonwebtoken";
 
-export const creatJwtToken = (userPayload) => {
-  return jwt.sign(userPayload, process.env.JWT_SECRET, {
+export const creatJwtToken = ({ payload }) => {
+  return jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
   });
 };
 
-export const createCookie = async (res, user) => {
-  const accessToken = creatJwtToken(user);
-  const cookieExpiryTime = 1000 * 60 * 60 * 24;
+export const createCookie = async (res, user, userRefreshToken) => {
+  const accessToken = creatJwtToken({ payload: { user } });
+  const refreshToken = creatJwtToken({ payload: { user, userRefreshToken } });
+
+  const accessTokenExpiryTime = 1000 * 60 * 15;
+  const refreshTokenExpiryTime = 1000 * 60 * 60 * 24 * 30;
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    expires: new Date(Date.now() + cookieExpiryTime),
+    expires: new Date(Date.now() + accessTokenExpiryTime),
     secure: process.env.NODE_ENV === "production",
     signed: true,
   });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+    expires: new Date(Date.now() + refreshTokenExpiryTime),
+  });
 };
 
-export const verifyAccessToken = async (authToken) =>
+export const verifyAccessToken = (authToken) =>
   jwt.verify(authToken, process.env.JWT_SECRET);
