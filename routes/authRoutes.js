8e491cf2742f621cb5645eb92f372b0c850a8537
Harish0705 <harish.balachandar@nzme.co.nz>
2024-09-24 10:06:13 +1200

    create verify-email, reset and forgot password routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,9 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  verifyEmail,
+  resetPassword,
+  forgotPassword
 } from "../controllers/authController.js";
 
 const authRouter = Router();
@@ -10,5 +13,8 @@ const authRouter = Router();
 authRouter.route("/register").post(registerUser);
 authRouter.route("/login").post(loginUser);
 authRouter.route("/logout").post(logoutUser);
+authRouter.post('/verify-email/', verifyEmail);
+authRouter.post('/reset-password', resetPassword);
+authRouter.post('/forgot-password', forgotPassword);
 
 export default authRouter;
