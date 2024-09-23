60147a3b65d7eb014e615def1ecaf1d86b1a2e69
Harish0705 <harish.balachandar@nzme.co.nz>
2024-09-24 10:08:03 +1200

    update auth middleware to check for refresh token

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,32 +1,48 @@
 import { UnauthenticatedError, UnauthorizedError } from "../errors/index.js";
 import { verifyAccessToken } from "../utils/index.js";
+import { Token } from "../models/userToken.js";
+import { hashToken } from "../utils/index.js";
 
 const authUserMiddleware = async (req, res, next) => {
-  const accessToken = req.signedCookies.accessToken;
-  if (!accessToken) {
-    throw new UnauthenticatedError("Invalid credentials");
-  }
+  const { accessToken, refreshToken } = req.signedCookies;
+
   try {
-    const userPayload = await verifyAccessToken(accessToken);
+   if (accessToken) {
+      const payload = verifyAccessToken(accessToken);
+      req.user = {
+        userId: payload.user.userId,
+        userName: payload.user.name,
+        userRole: payload.user.role,
+      };
+      return next();
+    }
+    const payload = verifyAccessToken(refreshToken);
+
+    const existingToken = await Token.findOne({
+      user: payload.user.userId,
+      refreshToken:payload.userRefreshToken,
+    });
+
+    if (!existingToken || !existingToken?.isValid) {
+      throw new UnauthenticatedError("Authentication Invalid");
+    }
+
     req.user = {
-      userId: userPayload.userId,
-      userName: userPayload.name,
-      userRole: userPayload.role,
+      userId: payload.user.userId,
+      userName: payload.user.name,
+      userRole: payload.user.role,
     };
-    // console.log(req.user);
+    console.log(req.user)
     next();
-  } catch (err) {
-    // console.log(error)
-    throw new UnauthenticatedError("Authentication failed");
+  } catch (error) {
+    throw new UnauthenticatedError("Invalid credentials");
   }
 };
 
 const authorizePermission = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.userRole)) {
-      throw new UnauthorizedError(
-        "Unauthorized to access this url"
-      );
+      throw new UnauthorizedError("Unauthorized to access this url");
     }
     next();
   };
